f2325a2955f0c583c1439f635cffc272783b384d
Rita isabel Pereira sampaio d'andrade marques <ripereir@c1r11s5.42lisboa.com>
2021-11-11 22:18:57 +0000

    new bonus function: ft_lstlast.c

---
+++ ft_lstlast.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstlast.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ripereir <ripereir@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/11 22:06:12 by ripereir          #+#    #+#             */
+/*   Updated: 2021/11/11 22:16:39 by ripereir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstlast(t_list *lst)
+{
+	t_list	*ls;
+
+	ls = lst;
+	if (ls == NULL)
+		return (NULL);
+	if (ls->next == NULL)
+		return (ls);
+	while (ls != NULL)
+	{
+		if (ls->next == NULL)
+			return (ls);
+		ls = ls->next;
+	}
+	return (NULL);
+}
