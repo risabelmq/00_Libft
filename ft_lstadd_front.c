2b542c11b540b40cf29f6d95cacb9f2cb64bd105
Rita isabel Pereira sampaio d'andrade marques <ripereir@c1r11s5.42lisboa.com>
2021-11-11 22:17:45 +0000

    included header in  ft_lstadd_front.c

--- ft_lstadd_front.c
+++ ft_lstadd_front.c
@@ -6,9 +6,11 @@
 /*   By: ripereir <ripereir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/11 21:29:08 by ripereir          #+#    #+#             */
-/*   Updated: 2021/11/11 21:34:02 by ripereir         ###   ########.fr       */
+/*   Updated: 2021/11/11 22:17:14 by ripereir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
 
 void	ft_lstadd_front(t_list **lst, t_list *new)
 {
